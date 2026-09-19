6c6fcbfaba665f01707a192b04bdca1de04ea4d4
Whitespace404 <rahulreji314@gmail.com>
2026-09-19 12:08:38 +0530

    remove python

--- app.py
+++
@@ -1,4 +0,0 @@
-from webs import app
-
-if __name__ == "__main__":
-    app.run(debug=True)
--- webs/routes.py
+++
@@ -1,9 +0,0 @@
-from flask import render_template
-
-from webs import app
-
-
-@app.route("/")
-@app.route("/home")
-def home():
-    return render_template("index.html")
